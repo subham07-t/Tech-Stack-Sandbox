e0ed186c9f0ea4b807180f10bf873f7755cbbb8d
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-05-06 22:09:04 +0530

    useMemo hook

--- React/components/useCallbackHook/ParentComponent.jsx
+++ React/components/useCallbackHook/ParentComponent.jsx
@@ -1,5 +1,7 @@
 // useCallback is a hook that will return a memorized version of the callback function that only changes if one of the dependencies has changed.in short useCallback is a React Hook that lets you cache a function definition between re-renders.
 
+// on the other hand useMemo is used to memoize the result of an expensive computation or function call. When a component is re-rendered, useMemo will only recompute the result if the inputs to the function have changed. Otherwise, it will return the cached result from the previous render.
+
 import React, { useState, useCallback } from "react";
 import Tittle from "./Tittle";
 import Count from "./Count";
