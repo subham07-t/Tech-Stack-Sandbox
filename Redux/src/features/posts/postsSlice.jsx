6d0c9d6729d272b6909784354a8c40ebca6d09cd
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-05-21 00:40:26 +0530

    submit time added of each post

--- Redux/src/features/posts/postsSlice.jsx
+++ Redux/src/features/posts/postsSlice.jsx
@@ -1,12 +1,19 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: "1",
     title: "Enhancing with Redux Toolkit",
     content: "Exceptional content",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
-  { id: "2", title: "Perfecting Slices", content: "Nailing it with finesse" },
+  {
+    id: "2",
+    title: "Perfecting Slices",
+    content: "Nailing it with finesse",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+  },
 ];
 
 const postsSlice = createSlice({
@@ -23,6 +30,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
           },
         };
