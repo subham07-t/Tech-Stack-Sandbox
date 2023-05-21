447795e9db8db926487c56c0ef0b1003e61c6f13
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-05-21 23:41:38 +0530

    fetch post and user using createAsyncThunk adn using extrareducers concept

--- Redux/src/main.jsx
+++ Redux/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import { fetchUsers } from "../src/features/users/userSlice.jsx";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
