f1fe645876fcccf8b9eb391813b019bebb6410aa
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-05-21 23:54:56 +0530

    createasyncthunk done

--- Redux/src/features/posts/postsSlice.jsx
+++ Redux/src/features/posts/postsSlice.jsx
@@ -15,6 +15,14 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   return response.data;
 });
 
+export const addNewPost = createAsyncThunk(
+  "posts/addNewPost",
+  async (initialPost) => {
+    const response = await axios.post(BASE_URL, initialPost);
+    return response.data;
+  }
+);
+
 const postsSlice = createSlice({
   name: "posts",
   initialState,
@@ -77,32 +85,26 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
-      });
-    // .addCase(addNewPost.fulfilled, (state, action) => {
-    //   // Fix for API post IDs:
-    //   // Creating sortedPosts & assigning the id
-    //   // would be not be needed if the fake API
-    //   // returned accurate new post IDs
-    //   const sortedPosts = state.posts.sort((a, b) => {
-    //     if (a.id > b.id) return 1;
-    //     if (a.id < b.id) return -1;
-    //     return 0;
-    //   });
-    //   action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
-    //   // End fix for fake API post IDs
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        const sortedPosts = state.posts.sort((a, b) => {
+          if (a.id > b.id) return 1;
+          if (a.id < b.id) return -1;
+          return 0;
+        });
+        action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
 
-    //   action.payload.userId = Number(action.payload.userId);
-    //   action.payload.date = new Date().toISOString();
-    //   action.payload.reactions = {
-    //     thumbsUp: 0,
-    //     hooray: 0,
-    //     heart: 0,
-    //     rocket: 0,
-    //     eyes: 0,
-    //   };
-    //   console.log(action.payload);
-    //   state.posts.push(action.payload);
-    // });
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp: 0,
+          hooray: 0,
+          heart: 0,
+          rocket: 0,
+          eyes: 0,
+        };
+        state.posts.push(action.payload);
+      });
   },
 });
 
